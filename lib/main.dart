f874cc2cd377fa20c0eba1133b8eb0c3c8bba886
yadhu <yadhusanil26@gmail.com>
2022-10-03 19:59:30 +0530

    appbar 😂

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:netflix_clone/home_page.dart';
+import 'package:netflix_clone/pages/lobby.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,18 +9,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
- 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily:GoogleFonts.montserrat().fontFamily ,
         brightness: Brightness.dark,
-        
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home:  HomePage(),
     );
   }
 }
-
